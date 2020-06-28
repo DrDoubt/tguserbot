34972c729b84ee580f42230877643cc8410f2f27
Narek Torosyan <narek.email@gmail.com>
2020-06-28 13:58:22 +0400

    Create flasher.py

---
+++ tg_userbot/modules/flasher.py
@@ -0,0 +1,25 @@
+import time
+import random
+
+from tg_userbot import CMD_HELP
+from tg_userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^.flash(.*)")
+async def flasher(event):
+	if not event.text[0].isalpha() and event.text[0] in ("."):
+		r = random.randint(1, 10000)
+		text = event.pattern_match.group(1)
+		await event.edit(f"`Flashing` {text}.zip`...`")
+		time.sleep(4)
+		if r % 2 == 1:
+			await event.edit(f"`Successfully flashed` {text}.zip`!`")
+		elif r % 2 == 0:
+			await event.edit(f"`Flashing` {text}.zip `failed successfully!`")
+		
+		
+
+CMD_HELP.update({
+    "flasher":
+        ".flash <text>\
+    \nUsage: Flash the .zip file."})
